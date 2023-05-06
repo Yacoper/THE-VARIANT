55dd7fac9961b5d5ef4665519eb4a750839638f6
LordPapkin <papkin2013@gmail.com>
2023-05-06 14:17:12 +0200

    modify test cubes, modify cubes data

--- Assets/Code/Items/Cubes/RedCubeDataSO.cs
+++ Assets/Code/Items/Cubes/RedCubeDataSO.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/Cubes/CubeData", fileName = "Red Cube Data")]
+[CreateAssetMenu(menuName = "ScriptableObjects/Cubes/RedCubeData", fileName = "RedCubeData")]
 public class RedCubeDataSO : CubeDataSO
 {
-    
+    [field: SerializeField] public LayerMask MoveableObjectLayerMask { get; private set; }
 }
