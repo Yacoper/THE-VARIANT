bc136d0ed8d95f6bb3bd2bde0f70a82648070c3c
LordPapkin <papkin2013@gmail.com>
2023-05-06 15:38:23 +0200

    apply movement cubes values from data

--- Assets/Code/Items/Cubes/BlueCubeDataSO.cs
+++ Assets/Code/Items/Cubes/BlueCubeDataSO.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Cubes/BlueCubeData", fileName = "BlueCubeData")]
 public class BlueCubeDataSO : CubeDataSO
 {
-    
+    [field: SerializeField] public float BuffDuration { get; private set; }
+    [field: SerializeField] public float JumpForceMultiplier { get; private set; }
 }
--- Assets/Code/Items/Cubes/GreenCubeDataSO.cs
+++ Assets/Code/Items/Cubes/GreenCubeDataSO.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Cubes/GreenCubeData", fileName = "GreenCubeData")]
 public class GreenCubeDataSO : CubeDataSO
 {
-    
+    [field: SerializeField] public float BuffDuration { get; private set; }
+    [field: SerializeField] public float PlayerSpeedMultiplier { get; private set; }
 }
