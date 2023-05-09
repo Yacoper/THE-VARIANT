8842cbd08f6996f33c40f446f9b38496f8fae7c6
Estremo102 <dominikmozdzen@gmail.com>
2023-05-09 20:26:29 +0200

    Options Placeholder

--- Assets/Code/Menu/MainMenu.cs
+++ Assets/Code/Menu/MainMenu.cs
@@ -5,10 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private int ActiveOptions = 0;
+    [SerializeField] private GameObject Options;
+    [SerializeField] private GameObject[] AdvancedOptions;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void OpenOptions()
+    {
+        ActiveOptions++;
+    }
+
+    public void Back()
+    {
+        if (ActiveOptions == 0) return;
+        switch (--ActiveOptions)
+        {
+            case 0:
+                Options.SetActive(false);
+                break;
+            case 1:
+                foreach(GameObject option in AdvancedOptions)
+                    option.SetActive(false);
+                break;
+        }
+    }
+
+
 
     public void QuitGame()
     {
