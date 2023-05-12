09e4594db0878de56709e678714170e3be01f50b
Estremo102 <dominikmozdzen@gmail.com>
2023-05-12 13:12:35 +0200

    MainMenu Placeholder

--- Assets/Code/Menu/PauseMenu.cs
+++ Assets/Code/Menu/PauseMenu.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [Header("Input")]
     [SerializeField] private InputReader inputReader;
     [SerializeField] private GameObject PauseVisual;
-    private bool paused = false;
+    [SerializeField] private GameObject OptionsVisual;
+    private bool Paused = false;
+    private int Options = 0;
 
     private void OnEnable()
     {
@@ -16,14 +19,67 @@ public class PauseMenu : MonoBehaviour
         inputReader.PauseGameAction += Pause;
     }
 
-    private void Pause(InputAction.CallbackContext callbackContext)
+    private void Pause(InputAction.CallbackContext callbackContext) => Pause();
+
+    public void Pause()
     {
-        paused = !paused;
-        PauseVisual.SetActive(paused);
-        if(paused)
+        if(Options > 0)
+        {
+            switch(--Options)
+            {
+                case 0:
+                    OptionsVisual.SetActive(false);
+                    break;
+                case 1:
+                    break;
+            }
+            return;
+        }
+        Paused = !Paused;
+        PauseVisual.SetActive(Paused);
+        Cursor.visible = true;
+        if (Paused)
             Time.timeScale = 0f;
         else
             Time.timeScale = 1f;
     }
 
+    public void Resume()
+    {
+        OpenOptions(false);
+        Pause();
+    }
+
+    public void OpenOptions(bool open)
+    {
+        if(!open)
+        {
+            Options = 0;
+            OptionsVisual.SetActive(false);
+            return;
+        }
+        switch(Options)
+        {
+            case 0:
+                Options++;
+                OptionsVisual.SetActive(true);
+                break;
+            case 1:
+                Options = 0;
+                OptionsVisual.SetActive(false);
+                break;
+        }
+    }
+
+    public void BackToMenu() =>
+        SceneManager.LoadScene(0);
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
