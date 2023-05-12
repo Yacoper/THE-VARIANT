b17c0d7b44b04f64ddba230df5d42e1a61a214ac
Daniel Pawlikowski <danio4242@gmail.com>
2023-05-13 00:00:16 +0200

    some fixes before test build

---
+++ Assets/Code/Triggers/AnimatorEnable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorEnable : MonoBehaviour
+{
+
+    public BoxCollider BoxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_Animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (BoxCollider.enabled == false)
+        {
+            BoxCollider.enabled = true;
+        }
+        else
+            BoxCollider.enabled = false;
+    }
+}
