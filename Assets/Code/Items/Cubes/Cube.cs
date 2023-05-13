332ad3cb24a935b599ada2935f4d3655c81d58be
LordPapkin <papkin2013@gmail.com>
2023-05-13 15:46:39 +0200

    change animations, toggle buff vfx, change pickup logic

--- Assets/Code/Items/Cubes/Cube.cs
+++ Assets/Code/Items/Cubes/Cube.cs
@@ -1,4 +1,4 @@
-using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class Cube : PickUpAbleItem
@@ -17,10 +17,35 @@ public class Cube : PickUpAbleItem
     
     [SerializeField] private BuffTypes buffType;
     [SerializeField] private CubeDataSO cubeData;
+    [SerializeField] private float desiredTime = 2f;
+    [SerializeField] private Vector3 desiredScale = new Vector3(0.5f, 0.5f, 0.5f);
 
+
+    public override void PickUp(Transform pickUpTargetTransform)
+    {
+        Transform cubeTransform = transform;
+        
+        itemTargetTransform = pickUpTargetTransform;
+        itemRigidbody.isKinematic = true;
+
+        cubeTransform.parent = itemTargetTransform;
+        transform.DOScale(desiredScale, desiredTime);
+        transform.DOMove(pickUpTargetTransform.position, desiredTime);
+    }
+
+    public override void Drop()
+    {
+        transform.parent = null;
+        itemTargetTransform = null;
+        itemRigidbody.isKinematic = false;
+        transform.DOScale(Vector3.one * 3f, 1f);
+    }
+
+    
     private void OnValidate()
     {
         ValidateUtilities.NullCheckVariable(this, nameof(buffType), buffType, true);
         ValidateUtilities.NullCheckVariable(this, nameof(cubeData), cubeData, true);
+        ValidateUtilities.NullCheckVariable(this, nameof(desiredTime), desiredTime, true);
     }
 }
